89d02deb9f2d5388025caa0660be0c172880d664
Jakub Maslowski <jakubmaslowski@Jakubs-Air.home>
2021-01-30 22:52:25 +0100

    apollo server express setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Session } from './entities/Session';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
 import microConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const session = orm.em.create(Session, { title: 'my first title' });
-  await orm.em.persistAndFlush(session);
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('\x1b[34m', '🏄🏻‍♂️  server started on localhost:4000 ');
+  });
 };
 
 main();
